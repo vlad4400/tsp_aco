91a237869221d3dc10f3e88b101cd73e03409694
vlad4400 <sleepvs@gmail.com>
2024-08-07 21:15:24 +0200

    import mongoose schemas

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -9,11 +9,17 @@ import { StudentService } from './services/student.service';
 import { MessageService } from './services/message.service';
 import { StudentRepository } from './repositories/student.repository';
 import { MessageRepository } from './repositories/message.repository';
+import { Student, StudentSchema } from './schemas/student.schema';
+import { Message, MessageSchema } from './schemas/message.schema';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URI),
+    MongooseModule.forFeature([
+      { name: Student.name, schema: StudentSchema },
+      { name: Message.name, schema: MessageSchema },
+    ]),
   ],
   controllers: [AppController, StudentController, MessageController],
   providers: [
