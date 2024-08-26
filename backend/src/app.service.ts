df26fc8450e31b28bf2fc09c2f476c72376c871a
vlad4400 <sleepvs@gmail.com>
2024-08-26 19:42:09 +0200

    backend: app text

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   appIsWorking(): string {
-    return 'App is running!';
+    return 'Backend is running!';
   }
 }
