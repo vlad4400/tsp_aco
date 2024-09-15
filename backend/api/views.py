a218af89b422533188e6a12029bd84b2c946bc7a
vlad4400 <sleepvs@gmail.com>
2024-09-15 20:46:06 +0200

    backend: add simple endpoint GET /api/hello/

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
-
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 @api_view(['GET'])
 def hello_world(request):
